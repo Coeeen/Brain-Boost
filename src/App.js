5b13f1eba081c568d0368871a58f3911fafd8432
Coeeen <Krystanjank@gmail.com>
2023-07-13 13:06:21 +0200

    build project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import questions from "./questions";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlashCards />
     </div>
   );
 }
 
-export default App;
+function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null);
+  const [flashcards, setFlashcards] = useState(questions);
+  const [questionInput, setQuestionInput] = useState("");
+  const [answerInput, setAnswerInput] = useState("");
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+  function ClearFlashcards(e) {
+    e.preventDefault();
+    setFlashcards([]);
+  }
+  function AddFlashcards(e) {
+    e.preventDefault();
+
+    if (questionInput === "" || answerInput === "") {
+      return;
+    }
+
+    setFlashcards((prevFlashcards) => [...prevFlashcards, newFlashcard]);
+    setQuestionInput("");
+    setAnswerInput("");
+
+    const newFlashcard = {
+      id: Date.now(),
+      question: questionInput,
+      answer: answerInput,
+    };
+  }
+  return (
+    <div>
+      <form className="Form">
+        <input
+          type="text"
+          placeholder="QUESTION"
+          value={questionInput}
+          onChange={(e) => setQuestionInput(e.target.value)}
+        ></input>
+
+        <input
+          type="text"
+          placeholder="ANSWER"
+          value={answerInput}
+          onChange={(e) => setAnswerInput(e.target.value)}
+        ></input>
+        <button onClick={AddFlashcards}>Add</button>
+        <button onClick={ClearFlashcards}>Clear</button>
+      </form>
+      <div className="flashcards">
+        {flashcards.map((question) => (
+          <div
+            key={question.id}
+            onClick={() => handleClick(question.id)}
+            className={question.id === selectedId ? "selected" : ""}
+          >
+            <p>
+              {question.id === selectedId ? question.answer : question.question}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/questions.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronized with state?",
+    answer: "Controlled element",
+  },
+  {
+    id: 4321,
+    question: "What is the purpose of React Router?",
+    answer: "To handle routing/navigation in React applications.",
+  },
+  {
+    id: 6789,
+    question: "What is the significance of the key prop in React lists?",
+    answer:
+      "The key prop helps React identify which items have changed, been added, or been removed in a list.",
+  },
+  {
+    id: 9876,
+    question: "What is the virtual DOM in React?",
+    answer:
+      "The virtual DOM is a lightweight copy of the actual DOM used by React to optimize rendering performance.",
+  },
+];
+
+export default questions;
